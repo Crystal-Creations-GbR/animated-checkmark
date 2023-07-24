2adc34df1ed6084e0d4b9bacc595c961e0757cfc
Johannes Huther <dev.johannes@huther.link>
2023-07-24 21:55:04 +0200

    feat: provide package as nuxt module
    Provide this package as a nuxt module for easier usage in nuxt 3 projects. Refer to `README.md` section "Usage in nuxt 3".

---
+++ src/nuxt.mjs
@@ -0,0 +1,12 @@
+import { defineNuxtModule, createResolver, addComponent } from "@nuxt/kit";
+
+export default defineNuxtModule({
+  async setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+
+    await addComponent({
+      name: "AnimatedCheckmark",
+      filePath: resolver.resolve("src/components/AnimatedCheckmark.vue"),
+    });
+  },
+});
