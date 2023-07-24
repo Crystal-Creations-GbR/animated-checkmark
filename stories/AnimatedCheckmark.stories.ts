163fc3ff480083dcdde26c2d941596c6cfb3c8ea
Johannes Huther <dev.johannes@huther.link>
2023-07-24 22:01:40 +0200

    test: add basic storybook setup
    Add basic storybook setup with an `AnimatedCheckmark` story.

---
+++ stories/AnimatedCheckmark.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import { AnimatedCheckmark } from "../src";
+
+const meta: Meta<typeof AnimatedCheckmark> = {
+  component: AnimatedCheckmark,
+  argTypes: {
+    color: {
+      control: { type: "color", presetColors: ["red", "green", "blue"] },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {
+  parameters: {
+    backgrounds: { default: "dark" },
+  },
+};
+
+export const Red: Story = {
+  args: {
+    color: "red",
+  },
+};
+
+export const Green: Story = {
+  args: {
+    color: "green",
+  },
+};
+
+export const Blue: Story = {
+  args: {
+    color: "blue",
+  },
+};
